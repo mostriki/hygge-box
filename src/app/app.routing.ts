4594f1e6c8fee4afefc51ac964f519be9c649a26
Sara Spink <spinkbot@gmail.com>
2017-11-06 11:50:42 -0800

    add api-keys

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,16 +6,22 @@ import { AdminComponent } from './admin/admin.component';
 import { AuthGuardService } from './auth-guard.service';
 import { UserComponent } from './user/user.component'
 import { HomepageComponent } from './homepage/homepage.component';
+import { ContactComponent } from './contact/contact.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
 
 const appRoutes: Routes = [
   {
      path: '',
      component: HomepageComponent
    },
-  // {
-  //   path: 'about',
-  //   component: AboutComponent
-  // },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'confirmation',
+    component: ConfirmationComponent
+  },
   {
     path: 'admin',
     component: AdminComponent,
