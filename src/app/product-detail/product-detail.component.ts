109c8ba986899b0b84fee486a54f168693a7198f
Karla Buckner and Sara Spink <karla+sara@>
2017-11-07 15:56:26 -0800

    rearrange routing to display each product on its own page properly by id

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,18 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute, Params  } from '@angular/router';
+import { FirebaseObjectObservable } from 'angularfire2/database';
+import { Location } from '@angular/common';
+import { ProductDetail } from '../product-detail.model';
+import { ProductDetailService } from '../productDetail.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.css'],
+  providers: [ProductDetailService]
 })
 export class ProductDetailComponent implements OnInit {
-  currentRoute: string = this.router.url;
+  boxId: string;
+  boxToDisplay;
 
-  constructor(private router: Router) { }
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private productDetailService: ProductDetailService
+  ) { }
 
   ngOnInit() {
-
+    this.route.params.forEach((urlParameters) => {
+      this.boxId = urlParameters['id'];
+  });
+  this.boxToDisplay = this.productDetailService.getBoxById(this.boxId);
   }
 
 }
