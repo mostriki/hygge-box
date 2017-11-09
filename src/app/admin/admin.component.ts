304635a83e8d8a4ba18a254ee7f0bf105cf107e6
Dawson Mortenson <dawson.mortenson@gmail.com>
2017-11-09 13:30:53 -0800

    add the admin page that loads contact messages

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,20 +1,44 @@
 import { Component } from '@angular/core';
 import * as firebase from "firebase";
+import { UserService } from "../user.service";
+import { ContactEntry } from '../contact-entry.model';
+import { ContactService } from '../contact.service';
+import { AuthenticationService }  from '../authentication.service';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css'],
-  providers: []
+  providers: [UserService, ContactService]
 })
 
 export class AdminComponent {
   private user;
+  private uid;
+  public messages = [];
 
-  constructor() { }
+  constructor(private userService: UserService, private contactService: ContactService, private authService: AuthenticationService) {
+    this.authService.user.subscribe(user => {
+        if (user == null) {
+        } else {
+          this.uid = user.uid;
+        }
+      });
+    }
+
+  ngOnInit() {
+    this.contactService.getContractEntries().subscribe(dataLastEmittedFromObserver => {
+      for (let message of dataLastEmittedFromObserver) {
+        this.messages.push(new ContactEntry(message.name, message.email, message.message));
+      }
+    })
+    this.userService.getUserById(this.uid).subscribe(dataLastEmittedFromObserver => {
+      this.user = dataLastEmittedFromObserver;
+     })
+  }
 
 
   ngDoCheck() {
-    this.user = firebase.auth().currentUser;
+    // this.user = firebase.auth().currentUser;
   }
 }
